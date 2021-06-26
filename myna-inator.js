8bfd86947124c278f40d1938018b36428cc59857
Omega-Centauri-21 <69707370+Omega-Centauri-21@users.noreply.github.com>
2021-06-26 16:48:29 +0530

    Update and rename main_na-inator.js to myna-inator.js

--- myna-inator.js
+++ myna-inator.js
@@ -1,7 +1,7 @@
-function maina(){
+function myna(){
     if (confirm("Hi !")){
         alert("My name is Nuuggettt ...");
-        alert("... and I am a big fat chiiickenn !!");
+        alert("... I am a big fat chiiickenn !!");
     }
     else{
 
